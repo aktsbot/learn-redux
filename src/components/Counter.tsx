3b5717b3bd92d7d57aaa1d8e27e2733f33da150f
aktsbot <box.ashishk@gmail.com>
2024-06-08 11:36:43 +0530

    async thunk added

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,11 +1,16 @@
 import { useDispatch, useSelector } from "react-redux";
 
-import { RootState } from "../state/store";
-import { decrement, increment } from "../state/counter/counterSlice";
+import { AppDispatch, RootState } from "../state/store";
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+  incrementAsync,
+} from "../state/counter/counterSlice";
 
 const Counter = () => {
   const count = useSelector((state: RootState) => state.counter.count);
-  const dispatch = useDispatch();
+  const dispatch = useDispatch<AppDispatch>();
 
   return (
     <div>
@@ -14,6 +19,12 @@ const Counter = () => {
       <div>
         <button onClick={() => dispatch(increment())}>Increment</button>
         <button onClick={() => dispatch(decrement())}>Decrement</button>
+        <button onClick={() => dispatch(incrementByAmount(10))}>
+          Increment by 10
+        </button>
+        <button onClick={() => dispatch(incrementAsync(120))}>
+          Increment async by 120
+        </button>
       </div>
     </div>
   );
